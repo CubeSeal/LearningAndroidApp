8d647ba4b29719cc60af92cb2fff5b039d5a400e
CubeSeal <kenndesilva1@gmail.com>
2026-03-15 19:43:22 +1100

    Let nix handle AndroidSDK.

--- flake.nix
+++ flake.nix
@@ -3,16 +3,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs {
+        inherit system;
+        config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
 
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          buildToolsVersions = [ "35.0.0" ];
+          platformVersions = [ "36" ];
+          includeEmulator = false;
+          includeSystemImages = false;
+          extraLicenses = [
+            "android-sdk-license"
+            "android-sdk-preview-license"
+          ];
+        };
+
+        androidSdk = androidComposition.androidsdk;
       in
       {
-        devShell = with pkgs; mkShell {
-          buildInputs = [ android-studio ];
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.android-studio androidSdk ];
+          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
         };
       }
     );
