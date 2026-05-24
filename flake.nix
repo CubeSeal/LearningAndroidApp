acdd5a7f1406c177c9ff8437fcfff9e541ac73f2
CubeSeal <kenndesilva1@gmail.com>
2026-05-24 15:37:02 +1000

    Get pkg for pixel vm.

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
-        inherit system;
-        config = {
+          inherit system;
+          config = {
             allowUnfree = true;
             android_sdk.accept_license = true;
           };
@@ -16,24 +17,38 @@
 
         androidComposition = pkgs.androidenv.composeAndroidPackages {
           buildToolsVersions = [ "35.0.0" ];
-          platformVersions = [ "36" ];
-          includeEmulator = false;
-          includeSystemImages = false;
+
+          # You can use "36", but see note below.
+          platformVersions = [ "35" "36" ];
+
+          includeEmulator = true;
+          includeSystemImages = true;
+
+          # This is the Play Store / Pixel-like image.
+          systemImageTypes = [ "google_apis_playstore" ];
+
+          # For x86_64 Linux desktop emulator.
+          abiVersions = [ "x86_64" ];
+
           extraLicenses = [
             "android-sdk-license"
             "android-sdk-preview-license"
+            "android-sdk-arm-dbt-license"
+            "intel-android-extra-license"
+            "intel-android-sysimage-license"
           ];
         };
 
         androidSdk = androidComposition.androidsdk;
       in
       {
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.android-studio
+            (pkgs.android-studio.withSdk androidSdk)
             androidSdk
             pkgs.kotlin-language-server
           ];
+
           ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
         };
